43bd1f849258dee59722b86697e9bff2e60e6d24
Suhayb Ali <91338345+Suhayb-Ali@users.noreply.github.com>
2021-11-05 01:56:52 +0000

    Factors

--- Main.java
+++ Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        
+      for(int i =1; i <= 100; i++) {
+        System.out.print(i + ": ");
+      for(int j = 1; j <= i; j++) {
+        if(i % j == 0){
+          System.out.print(j);
+          if(j != i) {
+            System.out.print(", ");
+          }
+        }
+      }
+      System.out.println("");
     }
+  }
 }
+
